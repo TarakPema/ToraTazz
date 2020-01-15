6819428255fa3239649a7174db6af91c3eb01747
TarakPema <59766804+TarakPema@users.noreply.github.com>
2020-01-15 13:04:56 +0200

    Day06 Ex00 - Ex04

---
+++ Day06/ex00/libft_creator.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+gcc -c ft_putchar.c ft_putstr.c ft_strcmp.c ft_swap.c ft_strlen.c
+ar rcs libft.a ft_putchar.o ft_putstr.o ft_swap.o ft_strlen.o ft_strcmp.o
---
+++ Day06/ex04/ft_sort_params.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpema <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/15 11:53:30 by tpema             #+#    #+#             */
+/*   Updated: 2020/01/15 12:15:13 by tpema            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	j = 1;
+	while (j < argc)
+	{
+		i = 0;
+		while (argv[j][i] != '\0')
+		{
+			if (argv[j][0] <= argv[j][i + 1])
+				ft_putchar(argv[i][j]);
+			i++;
+		}
+		ft_putchar('\n');
+		j++;
+	}
+	return (0);
+}
